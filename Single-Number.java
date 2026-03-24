762c2c5ed7e704ad86caed126744c6305b76c086
Dharan M J <23it1212@stjosephstechnology.ac.in>
2026-03-24 12:33:07 +0530

    [136] [Time Beats: 19.31%] [Memory Beats: 77.61%] - LeetPush

--- Single-Number.java
+++ Single-Number.java
@@ -1,9 +1,14 @@
 1class Solution {
 2    public int singleNumber(int[] nums) {
-3      int xor=0;
+3      HashMap<Integer,Integer> freq=new HashMap<>();
 4      for(int num : nums){
-5        xor^=num;
-6      }  
-7      return xor;
-8    }
-9}
+5        freq.put(num,freq.getOrDefault(num,0)+1);
+6      }
+7      for(Map.Entry<Integer,Integer> entry : freq.entrySet()){
+8        if(entry.getValue()==1){
+9            return entry.getKey();
+10        }
+11      }
+12      return -1;
+13    }
+14}
