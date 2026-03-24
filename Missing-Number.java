fb4927371aeb1fe89c31decde7f3a823a99f0748
Dharan M J <23it1212@stjosephstechnology.ac.in>
2026-03-24 10:18:07 +0530

    [268] [Time Beats: 100.00%] [Memory Beats: 43.12%] - LeetPush

--- Missing-Number.java
+++ Missing-Number.java
@@ -3,12 +3,11 @@
 3       int n=nums.length;
 4       int count=0;
 5       int count1=0;
-6       for(int i=0;i<n+1;i++){
+6       for(int i=0;i<n;i++){
 7            count+=i;
-8       }
-9       for(int i=0;i<n;i++){
-10           count1+=nums[i];
-11       }
-12       return count-count1;
-13    }
-14}
+8            count1+=nums[i];
+9       }
+10       count+=n;
+11       return count-count1;
+12    }
+13}
