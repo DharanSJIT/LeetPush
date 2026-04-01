7cab1d1656335de35c0bcc402e277174d7dbcc6e
Dharan M J <23it1212@stjosephstechnology.ac.in>
2026-04-01 13:04:10 +0530

    [509] [Time Beats: 44.07%] [Memory Beats: 24.12%] - LeetPush

--- Fibonacci-Number.java
+++ Fibonacci-Number.java
@@ -1,15 +1,6 @@
 1class Solution {
 2    public int fib(int n) {
-3        int[] dp=new int[n+1];
-4       
-5        if(n<=1) return n;
-6        if(n>=2){
-7             dp[0]=0;
-8             dp[1]=1;
-9        }
-10        for(int i=2;i<=n;i++){
-11            dp[i]=dp[i-1]+dp[i-2];
-12        }
-13        return dp[n];
-14    }
-15}
+3      if(n<=1) return n;
+4      return fib(n-1)+fib(n-2); 
+5    }
+6}
